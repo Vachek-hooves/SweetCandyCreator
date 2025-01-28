da65834061544be60f86d15748e31b5494a702a5
Vachek-hooves <vachek0602@gmail.com>
2025-01-28 16:15:47 +0200

    init sweet candies

---
+++ screen/menu/TabNavMenu.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabNavMenu = () => {
+  return (
+    <View>
+      <Text>TabNavMenu</Text>
+    </View>
+  )
+}
+
+export default TabNavMenu
+
+const styles = StyleSheet.create({})
