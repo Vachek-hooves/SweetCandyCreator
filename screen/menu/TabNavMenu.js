7ec895e1a1b275b5466342f6ba2f7a2e266e6ed6
Vachek-hooves <vachek0602@gmail.com>
2025-01-29 17:21:12 +0200

    tabbar

--- screen/menu/TabNavMenu.js
+++ screen/menu/TabNavMenu.js
@@ -1,75 +1,75 @@
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Home, Encyclopedia, Profile, Collections} from '../tab';
+import {Home, Encyclopedia, Profile} from '../tab';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavMenu = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
+        tabBarHideOnKeyboard: true,
         headerShown: false,
+        animation: 'fade',
         tabBarStyle: {
           height: 90,
-          paddingHorizontal: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          backgroundColor: '#FFE5FF',
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+          backgroundColor: '#fff',
+          borderTopWidth: 0,
         },
-        tabBarActiveTintColor: '#FDACFD',
-        tabBarInactiveTintColor: '#999999',
-      }}>
+        tabBarIcon: ({focused}) => {
+          let iconSource;
+
+          switch (route.name) {
+            case 'Home':
+              iconSource = require('../../assets/image/tabbar/Home.png');
+              break;
+            case 'Encyclopedia':
+              iconSource = require('../../assets/image/tabbar/Encyclopedia.png');
+              break;
+            case 'Profile':
+              iconSource = require('../../assets/image/tabbar/Profile.png');
+              break;
+          }
+
+          return (
+            <View style={[
+              styles.tabItem,
+              focused && styles.activeTabItem,
+            ]}>
+              <Image
+                source={iconSource}
+                style={[
+                  styles.icon,
+                  {tintColor: focused ? '#fff' : '#999999'},
+                ]}
+                resizeMode="contain"
+              />
+              {focused && (
+                <Text style={styles.activeLabel}>
+                  {route.name}
+                </Text>
+              )}
+            </View>
+          );
+        },
+        tabBarLabel: () => null,
+        tabBarItemStyle: {
+          padding: 0,
+        },
+      })}>
       <Tab.Screen
         name="Home"
         component={Home}
-        options={{
-          tabBarIcon: ({focused, color}) => (
-            <Image
-              source={require('../../assets/image/tabbar/Home.png')}
-              style={[styles.icon, {tintColor: color}]}
-              resizeMode="contain"
-            />
-          ),
-        }}
       />
-      {/* <Tab.Screen
-        name="Collections"
-        component={Collections}
-        options={{
-          tabBarIcon: ({focused, color}) => (
-            <Image
-              source={require('../../assets/image/tabbar/Collections.png')}
-              style={[styles.icon, {tintColor: color}]}
-              resizeMode="contain"
-            />
-          ),
-        }}
-      /> */}
       <Tab.Screen
         name="Encyclopedia"
         component={Encyclopedia}
-        options={{
-          tabBarIcon: ({focused, color}) => (
-            <Image
-              source={require('../../assets/image/tabbar/Encyclopedia.png')}
-              style={[styles.icon, {tintColor: color}]}
-              resizeMode="contain"
-            />
-          ),
-        }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
-        options={{
-          tabBarIcon: ({focused, color}) => (
-            <Image
-              source={require('../../assets/image/tabbar/Profile.png')}
-              style={[styles.icon, {tintColor: color}]}
-              resizeMode="contain"
-            />
-          ),
-        }}
       />
     </Tab.Navigator>
   );
@@ -78,9 +78,31 @@ const TabNavMenu = () => {
 export default TabNavMenu;
 
 const styles = StyleSheet.create({
+  tabItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 25,
+    minWidth: 45,
+    height: 45,
+    backgroundColor: '#F0F0F0',
+    marginTop:30
+  },
+  activeTabItem: {
+    backgroundColor: '#FDACFD',
+    paddingHorizontal: 16,
+    minWidth: 120,
+  },
   icon: {
-    width: 34,
-    height: 34,
-    marginBottom: 10,
+    width: 24,
+    height: 24,
+  },
+  activeLabel: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: '500',
+    marginLeft: 8,
+    marginTop: 5,
   },
 });
